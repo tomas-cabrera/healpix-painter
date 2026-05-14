018ccd7c4909a343935dd621652eccae91b23c97
tomas-cabrera <tcabrera@andrew.cmu.edu>
2026-05-14 13:20:33 -0400

    Added footprint plotting

--- src/healpix_painter/io/plotting.py
+++ src/healpix_painter/io/plotting.py
@@ -1,7 +1,6 @@
 # Plotting utilities
 
 import ligo.skymap.moc as lsm_moc
-import matplotlib.pyplot as plt
 import numpy as np
 import reproject
 from ligo.skymap import bayestar
@@ -19,7 +18,7 @@ FILTER2COLOR = {
 }
 
 
-def plot_skymap_gradient(ax, skymap_path, contours=[50, 90]):
+def plot_skymap_gradient(ax, skymap_path, imshow_kwargs={"cmap": "cylon"}):
     # Load skymap
     skymap = parse_skymap_args(skymap_filename=skymap_path)[1]
     if "UNIQ" in skymap.columns:
@@ -37,12 +36,12 @@ def plot_skymap_gradient(ax, skymap_path, contours=[50, 90]):
         nested=True,
     )
     img = np.ma.masked_array(img, mask=~mask.astype(bool))
-    ax.imshow(img, cmap="cylon")
+    ax.imshow(img, **imshow_kwargs)
 
     return None
 
 
-def plot_skymap_contours(ax, skymap_path, contours=[50, 90]):
+def plot_skymap_contours(ax, skymap_path, contours=[50, 90], plot_kwargs={}):
     # Load skymap
     skymap = parse_skymap_args(skymap_filename=skymap_path)[1]
     if "UNIQ" in skymap.columns:
@@ -61,7 +60,30 @@ def plot_skymap_contours(ax, skymap_path, contours=[50, 90]):
                 [v[0] for v in m],
                 [v[1] for v in m],
                 transform=ax.get_transform("world"),
-                label=f"{cv}",
+                **plot_kwargs,
             )
-    plt.legend()
     return None
+
+
+def plot_footprints(ax, footprint, scs, plot_kwargs={}):
+    # Iterate over skycoords
+    for sc in scs:
+        _region_coords = footprint.rotate(sc.ra.deg, sc.dec.deg)
+        # Iterate over CCDs:
+        for _ccd in _region_coords:
+            _x = _ccd[0, :]
+            _y = _ccd[1, :]
+            _x = np.append(_x, _x[0])
+            _y = np.append(_y, _y[0])
+            ax.plot(
+                _x,
+                _y,
+                transform=ax.get_transform("world"),
+                **plot_kwargs,
+            )
+        # NB: This uses the plotting provided by astropy.regions; slower, but may be better in the future
+        #       Presently does not handle the wrapping at the edges of Mollweide well
+        # _regions = footprint.regions_from_region_coords(region_coords=_region_coords)
+        # for _region in _regions:
+        #     _pixel_region = _region.to_pixel(WCS(ax.header))
+        #     _pixel_region.plot(ax=ax, **plot_kwargs)
--- tests/test_plotting/test_plotting.py
+++ tests/test_plotting/test_plotting.py
@@ -1,10 +1,14 @@
 import os
 import os.path as pa
 
+from astropy.table import Table
+from astropy.coordinates import SkyCoord
 import ligo.skymap.plot  # noqa: F401
 import matplotlib.pyplot as plt
 
 from healpix_painter.io import plotting
+from healpix_painter.telescopes.decam import DECamFootprint
+from healpix_painter.telescopes.ztf import ZTFFootprint
 
 # Get test data directory
 datadir = pa.join(
@@ -83,8 +87,84 @@ def test_plot_skymap_contours_zoom():
     assert True
 
 
+def test_plot_skymap_gradient_mollweide_coverage():
+    # Initialize axes
+    ax = plt.axes(projection="astro hours mollweide")
+    ax.grid()
+    # Plot skymap
+    plotting.plot_skymap_gradient(ax, S251112cm_skymap_filename)
+    # Plot DECam coverage
+    tab_decam = Table.read(S25111cm_pointings_decam_filename)
+    scs_decam = SkyCoord(
+        ra=tab_decam["ra"],
+        dec=tab_decam["dec"],
+        unit="deg",
+    )
+    plotting.plot_footprints(
+        ax,
+        DECamFootprint,
+        scs_decam,
+    )
+    # Plot ZTF coverage
+    tab_ztf = Table.read(S25111cm_pointings_ztf_filename)
+    scs_ztf = SkyCoord(
+        ra=tab_ztf["ra"],
+        dec=tab_ztf["dec"],
+        unit="deg",
+    )
+    plotting.plot_footprints(
+        ax,
+        ZTFFootprint,
+        scs_ztf,
+    )
+    plt.savefig("test_plot_skymap_gradient_mollweide_coverage.png")
+    plt.close()
+    assert True
+
+
+def test_plot_skymap_gradient_zoom_coverage():
+    # Initialize axes
+    ax = plt.axes(
+        projection="astro hours zoom",
+        center="0h -30d",
+        radius="25 deg",
+    )
+    ax.grid()
+    # Plot skymap
+    plotting.plot_skymap_gradient(ax, S251112cm_skymap_filename)
+    # Plot DECam coverage
+    tab_decam = Table.read(S25111cm_pointings_decam_filename)
+    scs_decam = SkyCoord(
+        ra=tab_decam["ra"],
+        dec=tab_decam["dec"],
+        unit="deg",
+    )
+    plotting.plot_footprints(
+        ax,
+        DECamFootprint,
+        scs_decam,
+    )
+    # Plot ZTF coverage
+    tab_ztf = Table.read(S25111cm_pointings_ztf_filename)
+    scs_ztf = SkyCoord(
+        ra=tab_ztf["ra"],
+        dec=tab_ztf["dec"],
+        unit="deg",
+    )
+    plotting.plot_footprints(
+        ax,
+        ZTFFootprint,
+        scs_ztf,
+    )
+    plt.savefig("test_plot_skymap_gradient_zoom_coverage.png")
+    plt.close()
+    assert True
+
+
 if __name__ == "__main__":
     test_plot_skymap_gradient_mollweide()
     test_plot_skymap_contours_mollweide()
     test_plot_skymap_gradient_zoom()
     test_plot_skymap_contours_zoom()
+    test_plot_skymap_gradient_mollweide_coverage()
+    test_plot_skymap_gradient_zoom()
