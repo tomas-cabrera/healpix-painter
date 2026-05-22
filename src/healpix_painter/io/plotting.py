33323bec8bcbfc52e817124c0fcf918fb4c5ad98
tomas-cabrera <tcabrera@andrew.cmu.edu>
2026-05-22 15:39:34 -0400

    Set plotting to use regions plotting

--- src/healpix_painter/io/plotting.py
+++ src/healpix_painter/io/plotting.py
@@ -3,6 +3,7 @@
 import ligo.skymap.moc as lsm_moc
 import numpy as np
 import reproject
+from astropy.wcs import WCS
 from ligo.skymap import bayestar
 
 from healpix_painter.healpix import calc_contours_for_skymap, parse_skymap_args
@@ -70,20 +71,7 @@ def plot_footprints(ax, footprint, scs, plot_kwargs={}):
     for sc in scs:
         _region_coords = footprint.rotate(sc.ra.deg, sc.dec.deg)
         # Iterate over CCDs:
-        for _ccd in _region_coords:
-            _x = _ccd[0, :]
-            _y = _ccd[1, :]
-            _x = np.append(_x, _x[0])
-            _y = np.append(_y, _y[0])
-            ax.plot(
-                _x,
-                _y,
-                transform=ax.get_transform("world"),
-                **plot_kwargs,
-            )
-        # NB: This uses the plotting provided by astropy.regions; slower, but may be better in the future
-        #       Presently does not handle the wrapping at the edges of Mollweide well
-        # _regions = footprint.regions_from_region_coords(region_coords=_region_coords)
-        # for _region in _regions:
-        #     _pixel_region = _region.to_pixel(WCS(ax.header))
-        #     _pixel_region.plot(ax=ax, **plot_kwargs)
+        _regions = footprint.regions_from_region_coords(region_coords=_region_coords)
+        for _region in _regions:
+            _pixel_region = _region.to_pixel(WCS(ax.header))
+            _pixel_region.plot(ax=ax, **plot_kwargs)
